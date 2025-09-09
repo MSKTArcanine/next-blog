4eff426e9b23455e89587d80f61a46a1ce99b6fe
Marc-AntoineSP <marcantoine.saintpierre@viacesi.fr>
2025-09-09 11:31:32 +0200

    ADD : flex page main editor/page.tsx

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -2,6 +2,7 @@ import EditorPost from "../ui/components/editorPost";
 
 export default function EditorPage(){
     return (
-        <EditorPost post="Titre incroivable" p="Je sais pas quoi dire mais c'est dingue en vrai"/>
-    )
+    <main className="flex flex-col flex-1 p-2">
+            <EditorPost post="Titre incroivable" p="Je sais pas quoi dire mais c'est dingue en vrai"/>
+    </main>)
 }
