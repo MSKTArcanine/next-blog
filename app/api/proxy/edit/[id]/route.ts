b4bf0a15d395e362af202813e8f923dba8cbfd44
Marc-AntoineSP <marcantoine.saintpierre@viacesi.fr>
2025-09-11 10:08:29 +0200

    Eh vasy c'est finit jpp

--- app/api/proxy/edit/[id]/route.ts
+++ app/api/proxy/edit/[id]/route.ts
@@ -14,3 +14,21 @@ export async function DELETE(req: Request, { params }:{params: {id:string}}){
         return Response.json({message: "Failed to delete"}, {status:res.status});
     return new Response(null, {status:204});
 }
+
+export async function PUT(req: Request, {params}:{params:{id:string}}){
+    const {id} = await params;
+    const session = await auth();
+    const b = await req.json();
+    console.log('PUT : ', b)
+    const res = await fetch(`https://blog-api-dqc2a0ftfra7akc5.francecentral-01.azurewebsites.net/api/v1/edit/post/${id}`,
+        {
+            method:'PUT',
+            headers:{'Authorization':`bearer ${session?.accessToken}`,
+        'Content-Type':'application/json'},
+            body:JSON.stringify(b),
+        }
+    )
+    if(!res.ok)
+        return Response.json({message:"Failed to UPDATE"}, {status:res.status});
+    return new Response(null, {status:201});
+}
--- app/ui/components/editorPost.tsx
+++ app/ui/components/editorPost.tsx
@@ -3,16 +3,32 @@
 import Image from "next/image"
 import React, { useState } from "react"
 import EditInput from "@/app/ui/components/editInput"
+import { title } from "process";
 
-export default function EditorPost(props:{post:string, p:string, id:number, onHandleDeleteClick:CallableFunction}){
+export default function EditorPost(props:{post:string, p:string, id:number, desc:string, isPublished:boolean, onHandleDeleteClick:CallableFunction, onHandleEdit:CallableFunction}){
     const [isEdit, setIsEdit] = useState<boolean>(false);
     const [post, setPost] = useState<string>(props.post);
     const [p, setP] = useState<string>(props.p);
+    const [desc, setDesc] = useState<string>(props.desc);
+    const [isPublished, setIsPublished] = useState<boolean>(props.isPublished);
     const onHandleEditClick = () => setIsEdit(prev => !prev);
     const onPostChange = (e:React.ChangeEvent<HTMLInputElement>) => setPost(e.currentTarget.value);
     const onPChange = (e:React.ChangeEvent<HTMLInputElement>) => setP(e.currentTarget.value);
-
+    const onHandleToggle = (e:React.ChangeEvent<HTMLInputElement>) => setIsPublished(prev => !prev);
     const deletePost = async () => await fetch(`/api/proxy/edit/${props.id}`, {method:"DELETE"});
+    const updatePost = async () => {
+        const res = await fetch(`/api/proxy/edit/${props.id}`, {
+        method:'PUT',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify({
+            title:post,
+            content:p,
+            desc:desc,
+            is_published:isPublished,
+        })
+    });
+    
+}
     return (
         <article className="p-2 flex flex-col gap-[2rem] border-2 border-accent-content rounded-2xl">
             <div className="flex flex-col gap-4">
@@ -21,11 +37,11 @@ export default function EditorPost(props:{post:string, p:string, id:number, onHa
             </div>
             <div className="buttonGroup flex flex-row flex-start">
                 <div className="groupe1 flex flex-row gap-2">
-                    <button type="button" className="btn btn-primary p-3 rounded-[8px]" onClick={onHandleEditClick}>{isEdit ? 'Save' : 'Edit'} <Image src={isEdit ? '/assets/icons8-check.svg' : '/assets/icons8-plus.svg'} alt='plus' width={12} height={12} className="dark:invert"></Image></button>
+                    <button type="button" className="btn btn-primary p-3 rounded-[8px]" onClick={async () => {if(isEdit) {await updatePost(); props.onHandleEdit(props.id, {title:post, content:p, desc:desc, is_published:isPublished})}; onHandleEditClick()}}>{isEdit ? 'Save' : 'Edit'} <Image src={isEdit ? '/assets/icons8-check.svg' : '/assets/icons8-plus.svg'} alt='plus' width={12} height={12} className="dark:invert"></Image></button>
                     <button type="submit" className="btn btn-secondary p-3 rounded-[8px]" onClick={() => {deletePost(); props.onHandleDeleteClick(props.id)}}>Delete <Image src={'/assets/minus-svgrepo-com.svg'} alt='plus' width={12} height={12} className="dark:invert"></Image></button>
                 </div>
                 <div className="groupe2 flex-1 flex justify-end">
-                    <label className=" text-white btn btn-secondary p-3 rounded-[8px] has-checked:btn-accent">Published<input type="checkbox" name="edit" id="edit" className={`toggle`} defaultChecked={true}/></label>
+                    <label className=" text-white btn btn-secondary p-3 rounded-[8px] has-checked:btn-accent">Published<input type="checkbox" name="edit" id="edit" className={`toggle`} defaultChecked={isPublished} onChange={onHandleToggle}/></label>
                 </div>
             </div>
         </article>
